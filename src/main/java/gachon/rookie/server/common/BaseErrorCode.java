ae852632b98f6c649667aecaff33fc0a11d5ae3f
destiny3912 <destiny3910@gmail.com>
2023-08-18 15:45:30 +0900

    [ADD] JWT Utils

--- src/main/java/gachon/rookie/server/common/BaseErrorCode.java
+++ src/main/java/gachon/rookie/server/common/BaseErrorCode.java
@@ -8,9 +8,14 @@ import org.springframework.http.HttpStatus;
  * */
 @Getter
 public enum BaseErrorCode {
+    //400
+    JWT_NOT_EXIST(HttpStatus.BAD_REQUEST, "JWT 존재하지 않음"),
 
+    //500
+    JWT_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "JWT ERROR"),
     DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Database Error"),
     UNEXPECTED_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Unexpected Error");
+
 
     private final HttpStatus code;
     private final String message;
